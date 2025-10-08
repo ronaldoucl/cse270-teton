f52c0fddd666cc5dcfd772e1e5bd32ba04756503
ronaldoucl <ronaldocl9lda@gmail.com>
2025-10-08 05:23:59 -0600

    smoke tests

--- teton/1.6/config/config.js
+++ teton/1.6/config/config.js
@@ -1,11 +1,11 @@
 const LAT = "43.8866";
 const LON = "-111.6777";
-const APIKEY = "<YOUR API KEY HERE>";
+const APIKEY = "fba8b683cf8d9d0e1e5b6c4eea50feee";
 
 const WEATHER_URL_STUB = "./data/weather.json";
 const WEATHER_URL_PROD = `https://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&appid=${APIKEY}`;
 
-const apiURL = WEATHER_URL_STUB;
+const apiURL = WEATHER_URL_PROD;
 
 const DIRECTORY_DATA_URL_PROD = "<PRODUCTION_URL_HERE>";
 const DIRECTORY_DATA_URL_STUB = "./data/business.json";
@@ -17,4 +17,4 @@ const USER_DATA_URL_PROD = "<PRODUCTION_URL_HERE>";
 const USER_DATA_URL_TEST = "http://127.0.0.1:8000/users/";
 const USER_DATA_URL_STUB = "./data/users.noexist.json";
 
-const userDataUrl = USER_DATA_URL_STUB;
+const userDataUrl = USER_DATA_URL_TEST;
