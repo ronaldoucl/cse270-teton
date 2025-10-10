9d55d32b75e6fad2ad409ef47aad8cf1ff69e61b
ronaldoucl <ronaldocl9lda@gmail.com>
2025-10-09 19:21:46 -0600

    headless test

--- teton/1.6/config/config.js
+++ teton/1.6/config/config.js
@@ -5,7 +5,7 @@ const APIKEY = "<YOUR API KEY HERE>";
 const WEATHER_URL_STUB = "./data/weather.json";
 const WEATHER_URL_PROD = `https://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&appid=${APIKEY}`;
 
-const apiURL = WEATHER_URL_PROD;
+const apiURL = WEATHER_URL_STUB;
 
 const DIRECTORY_DATA_URL_PROD = "<PRODUCTION_URL_HERE>";
 const DIRECTORY_DATA_URL_STUB = "./data/business.json";
@@ -17,4 +17,4 @@ const USER_DATA_URL_PROD = "<PRODUCTION_URL_HERE>";
 const USER_DATA_URL_TEST = "http://127.0.0.1:8000/users/";
 const USER_DATA_URL_STUB = "./data/users.noexist.json";
 
-const userDataUrl = USER_DATA_URL_TEST;
+const userDataUrl = USER_DATA_URL_STUB;
